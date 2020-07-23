7cb8c695216e054d3f2d55724c1f7aa920161ce0
darhao <darhao@qq.com>
2020-07-23 18:36:58 +0800

    添加了chart.js

--- src/main/java/cc/darhao/fund/main/Main.java
+++ src/main/java/cc/darhao/fund/main/Main.java
@@ -7,9 +7,7 @@ public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		//配置web服务器	
-		UndertowServer undertowServer = UndertowServer.create(UndertowBoot.class);
-		undertowServer.setPort(8080);
-		undertowServer.start();
+		UndertowServer.create(UndertowBoot.class).start();
 	}
 
 }
