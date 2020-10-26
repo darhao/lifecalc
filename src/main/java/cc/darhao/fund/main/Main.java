971eee6e868af8e4e59f85961aea8180efc49a5a
darhao <darhao@qq.com>
2020-10-26 20:04:28 +0800

    1

--- src/main/java/cc/darhao/fund/main/Main.java
+++ src/main/java/cc/darhao/fund/main/Main.java
@@ -6,8 +6,8 @@ import com.jfinal.server.undertow.UndertowServer;
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
-		//配置web服务器	
-		UndertowServer.create(UndertowBoot.class).start();
+		//配置web服务器
+		UndertowServer.create(UndertowBoot.class).setPort(8888).start();
 	}
 
 }
